c24095b22b6eae0a460700bf81f26e10c844ba1c
Fernando Pérez <gunderwulde@gmail.com>
2016-09-13 14:22:35 +0200

    - Pantalla de creditos.

---
+++ Assets/Scripts/Creditos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Creditos : MonoBehaviour {
+	public ScrollRect m_AreaDeScroll;
+	float m_posicion;
+	public void OnEnable(){
+		m_posicion=-0.1f;
+	}
+	public void Update(){
+		m_posicion += Time.deltaTime / 10;
+		if(m_posicion<0) m_AreaDeScroll.verticalNormalizedPosition = 1;
+		else if(m_posicion>1) m_AreaDeScroll.verticalNormalizedPosition = 0;
+		else m_AreaDeScroll.verticalNormalizedPosition = 1.0f - m_posicion;
+	}
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,7 +2,23 @@
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+	public GameObject m_Creditos;
+	public GameObject m_Menu;
+	void Start(){
+		OcultarCreditos();
+	}
 	public void OnPlay(){
 		SceneManager.LoadScene("Game");
+	}
+
+	public void MostarCreditos(){
+		m_Creditos.SetActive(true);
+		m_Menu.SetActive(false);
+		
+	}
+	public void OcultarCreditos(){
+		m_Creditos.SetActive(false);
+		m_Menu.SetActive(true);
+		
 	}
 }
